79542f5193268ab873890c80606670a5c97097ac
Selim Mammadov <mamedovfhn@gmail.com>
2022-09-01 21:19:41 +0400

    task-2 new

--- task-2/main.js
+++ task-2/main.js
@@ -6,6 +6,8 @@ prompt.get('input', function (err, result) {
 
     let numbers = result.input.split(" ");
     let max = parseInt(numbers[0]);
+    let index = 0
+    
     for (let i = 0; i < numbers.length; i++) {
         const element = parseInt(numbers[i]);
         if (element >= max) {
